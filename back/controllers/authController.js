16041c5e2fe552380e71428a0b8c7a1972d5fa9f
Yehor Pliasov <fenix4088@gmail.com>
2021-08-29 13:04:12 +0300

    feature: generate JWT backend when login

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -1,5 +1,6 @@
-const {USER} = require("../schemas/schemas");
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {USER} = require("../schemas/schemas");
 
 
 class AuthController {
@@ -7,17 +8,42 @@ class AuthController {
         try {
             const {login, password} = req.body;
             const candidate = await USER.findOne({name: login});
-            if(candidate) {
+            if (candidate) {
                 return res.status(400).json({message: 'User already exist'});
             } else {
                 const hashedPassword = bcryptjs.hashSync(password, 7);
                 const newUser = new USER({name: login, password: hashedPassword});
                 await newUser.save();
-                return res.status(200).send({message: 'User was create'});
+                return res.status(200).json({message: 'User was create'});
             }
         } catch (e) {
-
+            return res.status(500).json(e);
         }
+    }
+
+    async login(req, res) {
+        try {
+            const {login, password} = req.body;
+
+            const user = await USER.findOne({name: login});
+            const isPassword = bcryptjs.compareSync(password, user.password);
+
+            if (!user) {
+                return res.status(400).json({message: "Wrong login or password"});
+            }
+
+            if(!isPassword) {
+                return res.status(400).json({message: "Wrong login or password"});
+            }
+
+            const token = jwt.sign({userId: user._id}, process.env.TOKEN_SECRET, { expiresIn: '3600s' })
+
+            return res.status(200).json({message: "Login success", token});
+
+        } catch (e) {
+            return res.status(500).json({message: "Server error"});
+        }
+
     }
 }
 
--- back/roters/authRouter.js
+++ back/roters/authRouter.js
@@ -2,10 +2,11 @@ const Router = require("express");
 const AuthController = require("../controllers/authController");
 const router = new Router();
 
-router.use((req, res, next) => {
-    next();
-});
+// router.use((req, res, next) => {
+//     // next();
+// });
 
-router.post('/register', AuthController.register)
+router.post('/register', AuthController.register);
+router.post('/login', AuthController.login);
 
 module.exports = router;
